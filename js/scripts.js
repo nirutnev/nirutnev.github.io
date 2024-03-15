7e20ee33c6b6f33a50efff587b16882b275a8c8f
odious-desire <odious.desire.band@gmail.com>
2024-03-15 11:46:09 +0100

    saudades

--- js/scripts.js
+++ js/scripts.js
@@ -2,13 +2,13 @@
   const parallax1 = document.getElementsByClassName('parallax1');
 new simpleParallax(parallax1, {
     scale: 0.8,
-    delay: 0.5,
+    delay: 1.0,
 });
 
 const parallax2 = document.getElementsByClassName('parallax2');
 new simpleParallax(parallax2, {
-    scale: 0.95,
-    delay: 1,
+    scale: 0.9,
+    delay: 0.5,
 });
 
 const parallaxR = document.getElementsByClassName('parallaxR');
